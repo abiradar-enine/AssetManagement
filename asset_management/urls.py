bb5b5729f1035abd31fb48164b556127f70c25ef
Your Name <you@example.com>
2023-11-16 13:39:07 +0530

    Added UpdateView for Assets

--- asset_management/urls.py
+++ asset_management/urls.py
@@ -1,7 +1,8 @@
-from .views.asset_views import CreateAsset
+from .views.asset_views import CreateAsset, UpdateAsset
 from django.urls import path
 
 
 urlpatterns = [
     path("new_asset", CreateAsset.as_view(), name="add_new_asset"),
+    path("update_asset/<int:pk>", UpdateAsset.as_view(), name="update_asset"),
 ]
