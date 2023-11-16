9f53fd54b7964f8c2bde9e9f48898429c4eeaf21
Your Name <you@example.com>
2023-11-16 13:14:23 +0530

    Added CreateView for Asset

--- asset_management/views.py
+++ asset_management/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import AssetGroup, Asset, AssetAllocation
+from .forms import CreateAsset
 
-# Create your views here.
+
+class CreateAsset(CreateView):
+    template_name = 'addAsset.html'
+    model = Asset
+    success_url = ''
+    form_class = CreateAsset
